88d3bb5b1d544238f97a23344f18aadfeed6cf96
Vigneshvicky2008 <vigneshvicky06273@gmail.com>
2025-04-28 13:52:54 +0530

    config fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,21 +13,21 @@
 // 		},
 // 	},
 // });
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This ensures the output is in the "dist" folder
+  },
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+      '/api': {
+        target: 'http://localhost:5000',
       },
     },
-  },
-  build: {
-    outDir: "dist", // Specify the output directory as 'dist'
   },
 });
