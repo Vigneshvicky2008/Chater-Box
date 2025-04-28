e96c4561dcf6e0a76049eb44e14989a15384fe55
Vigneshvicky2008 <vigneshvicky06273@gmail.com>
2025-04-28 13:49:10 +0530

    Add build output directory configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,33 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		port: 3000,
+// 		proxy: {
+// 			"/api": {
+// 				target: "http://localhost:5000",
+// 			},
+// 		},
+// 	},
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
+  build: {
+    outDir: "dist", // Specify the output directory as 'dist'
+  },
 });
